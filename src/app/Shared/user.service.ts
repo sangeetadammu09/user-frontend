ba7046b32318ad99b0aacae2d6d0cda33e68be48
sangeetadammo09 <sangeetadammu12@gmail.com>
2023-06-03 16:47:59 +0530

    user-crud-fronend

---
+++ src/app/Shared/user.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http : HttpClient) { }
+
+  getAllUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${environment.apiUrl}/allusers`);
+  }
+
+  addUser(user:any): Observable<any[]> {
+    return this.http.post<any[]>(`${environment.apiUrl}/create-user`,user);
+  }
+
+  updateUser(id:any,user:any): Observable<any[]> {
+    return this.http.put<any[]>(`${environment.apiUrl}/update-user/${id}`,user);
+  }
+
+  deleteUser(id:any): Observable<any[]> {
+    return this.http.delete<any[]>(`${environment.apiUrl}/delete-user/${id}`);
+  }
+}

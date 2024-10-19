1b6d94cdd929b0242880c1d77576e2340a9d5acc
sangeetadammo09 <sangeetadammu12@gmail.com>
2024-10-19 18:17:51 +0530

    modified docker file

--- src/app/Components/add-user/add-user.component.ts
+++ src/app/Components/add-user/add-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { UserService } from 'src/app/Shared/user.service';
+import { UserService } from '../../Shared/user.service';
 
 @Component({
   selector: 'app-add-user',
--- src/app/Components/user-list/user-list.component.ts
+++ src/app/Components/user-list/user-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { UserService } from 'src/app/Shared/user.service';
+import { UserService } from '../../Shared/user.service';
 
 @Component({
   selector: 'app-user-list',
